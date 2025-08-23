042f2e5a94442f36c3aa2cf985ee155b260b5ff1
Jose P. Ferreira <jesep_ferreira@icloud.com>
2025-08-23 14:14:54 -0300

    feat: add patient entity

---
+++ patient-service/src/main/java/xyz/thaumazein/patientservice/entities/Patient.java
@@ -0,0 +1,37 @@
+package xyz.thaumazein.patientservice.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+}
