12196ac988c91be701545c3821f860bf1d40dde1
Jose P. Ferreira <jesep_ferreira@icloud.com>
2025-08-24 14:09:01 -0300

    build: add swagger configuration for API documentation

--- patient-service/src/main/java/xyz/thaumazein/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/xyz/thaumazein/patientservice/controller/PatientController.java
@@ -1,5 +1,7 @@
 package xyz.thaumazein.patientservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.groups.Default;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +18,18 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patient", description = "API for managing Patients")
 public class PatientController {
     private final PatientService patientService;
 
     @GetMapping
+    @Operation(summary = "Get Patients")
     public List<PatientResponse> getPatients() {
         return patientService.getPatients();
     }
 
     @PostMapping
+    @Operation(summary = "Create a new Patients")
     public ResponseEntity<PatientResponse> createPatient(@RequestBody
                                                          @Validated({Default.class, CreatePatientValidationGroup.class})
                                                          PatientRequest request) {
@@ -35,6 +40,7 @@ public class PatientController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update Patient")
     public ResponseEntity<PatientResponse> updatePatient(@PathVariable UUID id,
                                                          @RequestBody @Validated({Default.class}) PatientRequest request) {
 
@@ -44,6 +50,7 @@ public class PatientController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete Patient")
     public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
         patientService.deletePatient(id);
         return ResponseEntity.noContent().build();
